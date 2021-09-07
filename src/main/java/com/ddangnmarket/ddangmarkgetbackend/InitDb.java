cbb10931cec93b4c12c2758066d9bdbcd64242ab
SeunghyunYoo <dysh1017@gmail.com>
2021-09-07 12:40:49 +0900

    [Test] test 용 data 추가

--- src/main/java/com/ddangnmarket/ddangmarkgetbackend/InitDb.java
+++ src/main/java/com/ddangnmarket/ddangmarkgetbackend/InitDb.java
@@ -12,6 +12,8 @@ import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import java.util.List;
+
 import static com.ddangnmarket.ddangmarkgetbackend.domain.district.Dong.*;
 
 @Component
@@ -25,8 +27,8 @@ public class InitDb {
     public void init(){
         initService.initCategory();
         initService.initDistrict();
-        initService.initAccountPost();
-        initService.initAccount();
+//        initService.initAccountPost();
+        initService.initAccountPostV2();
     }
 
     @Component
@@ -107,7 +109,59 @@ public class InitDb {
             em.persist(chat3);
         }
 
-        public void initAccount(){
+        public void initAccountPostV2(){
+            Account account1 = new Account("account1", "000-0000-0000", "account1@gmail.com", "00000000");
+            Account account2= new Account("account2", "000-0000-0000", "account2@gmail.com", "00000000");
+            Account account3 = new Account("account3", "000-0000-0000", "account3@gmail.com", "00000000");
+
+            ActivityArea activeArea1 = ActivityArea.createActiveArea(districtJpaRepository.findByDong(Dong.GUMI), 0);
+            ActivityArea activeArea2 = ActivityArea.createActiveArea(districtJpaRepository.findByDong(JEONGJA1), 0);
+            ActivityArea activeArea3 = ActivityArea.createActiveArea(districtJpaRepository.findByDong(Dong.UNJUNG), 0);
+
+            account1.setActivityArea(activeArea1);
+            account2.setActivityArea(activeArea2);
+            account3.setActivityArea(activeArea3);
+
+            em.persist(account1);
+            em.persist(account2);
+            em.persist(account3);
+
+            Category digital = categoryJpaRepository.findByCategoryTag(CategoryTag.DIGITAL);
+            Category book = categoryJpaRepository.findByCategoryTag(CategoryTag.BOOK);
+            Category clothes = categoryJpaRepository.findByCategoryTag(CategoryTag.CLOTHES);
+            Category furniture = categoryJpaRepository.findByCategoryTag(CategoryTag.FURNITURE);
+
+            Post post1 = Post.createPost("맥북판매", "맥북팔아요", 100000, digital, account1);
+            em.persist(post1);
+            Post post2 = Post.createPost("스프링 책 판매", "스프링 책 팔아요", 10000, book, account1);
+            em.persist(post2);
+            Post post3 = Post.createPost("티셔츠 판매 ", "티셔츠 팔아요", 100000, clothes, account1);
+            em.persist(post3);
+            Post post4 = Post.createPost("책상 판매", "책상 팔아요", 10000, furniture, account1);
+            em.persist(post4);
+
+            int i = 0;
+            String title = "title";
+            String desc = "description";
+            for (Account account : List.of(account1, account2, account3)) {
+                for (CategoryTag categoryTag :CategoryTag.values()){
+                    Category category = categoryJpaRepository.findByCategoryTag(categoryTag);
+
+                    em.persist(
+                            Post.createPost(title + " " + i,
+                                    desc + " " + i +" "+ account.getNickname(),
+                                    10000, category, account));
+                    i+=1;
+                }
+            }
+
+            Chat chat1 = Chat.createChat(post1, account2);
+            em.persist(chat1);
+            Chat chat2 = Chat.createChat(post1, account3);
+            em.persist(chat2);
+
+            Chat chat3 = Chat.createChat(post2, account2);
+            em.persist(chat3);
         }
 
 

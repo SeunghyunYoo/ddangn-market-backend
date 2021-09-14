fb716315d8325e352be6af26635011cc2ff85562
SeunghyunYoo <dysh1017@gmail.com>
2021-09-14 20:27:04 +0900

    [Refactor] directory 구조 & chatStatus 원복

---
+++ src/main/java/com/ddangnmarket/ddangmarkgetbackend/domain/category/CategoryTag.java
@@ -0,0 +1,20 @@
+package com.ddangnmarket.ddangmarkgetbackend.domain.category;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum CategoryTag {
+    //생활 가전, 가구, 의류, 도서, 디지털기기, 기타
+    APPLIANCE, FURNITURE, CLOTHES, BOOK, DIGITAL, OTHERS;
+
+    @JsonCreator
+    public static CategoryTag fromString(String key){
+        for(CategoryTag categoryTag : CategoryTag.values()){
+            if(categoryTag.name().equalsIgnoreCase(key)){
+                return categoryTag;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 품목입니다.");
+    }
+
+}
